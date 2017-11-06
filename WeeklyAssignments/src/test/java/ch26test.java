80674a416d209195ddaec8dcc520076a3918acba
ThuyVy Tran <tmt2039@email.vccs.edu>
2017-11-06 13:37:39 -0500

    new files

--- WeeklyAssignments/src/test/java/ch26test.java
+++ WeeklyAssignments/src/test/java/ch26test.java
@@ -1,12 +1,50 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import org.junit.Test;
 
-public class ch26test {
+import ch26.AVLTree;
+import ch26.BST;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+public class ch26test {
+	public static void main(String[] args) {
+		  ArrayList<Integer> data = new ArrayList<>();
+		  for (int i = 0; i < 500000; i++) {
+		   data.add(i);
+		  }
+		  Collections.shuffle(data);
+
+		  AVLTree<Integer> avlTree = new AVLTree<>();
+		  BST<Integer> bst = new BST<>();
+		  
+		  long time = System.currentTimeMillis();
+		  for (Integer integer : data) {
+		   bst.insert(integer);
+		  }
+		  System.out.println("BST insert = " + (System.currentTimeMillis() - time));
+		  
+		  time = System.currentTimeMillis();
+		  for (Integer integer : data) {
+		   avlTree.insert(integer);
+		  }
+		  System.out.println("AVLTree insert = " + (System.currentTimeMillis() - time));
+
+		  Collections.shuffle(data);
+		  
+		  time = System.currentTimeMillis();
+		  for (Integer integer : data) {
+		   bst.search(integer);
+		  }
+		  System.out.println("BST search = " + (System.currentTimeMillis() - time));
+
+		  time = System.currentTimeMillis();
+		  for (Integer integer : data) {
+		   avlTree.search(integer);
+		  }
+		  System.out.println("AVLTree search = " + (System.currentTimeMillis() - time));
+
+		 }
 
 }
